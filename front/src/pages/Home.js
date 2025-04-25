4ace17f0bfad1df104c0f0750a06d4898028364c
chennissan <chen.nissan@gmail.com>
2025-04-25 10:48:32 +0300

    styling and clean

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -5,9 +5,9 @@ import styles from "../styles/theme.module.css";
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
-      {/* <h1>Lost & Found App Introduction</h1> */}
+      <h1>Lost & Found App Introduction</h1>
       <video className={styles.homeVideo} autoPlay muted playsInline>
-        <source src="pkf1.mp4" type="video/mp4" />
+        <source src="lnf1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
